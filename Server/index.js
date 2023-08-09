27b617f692f32cb712f09f88e8076d3c94831061
Cal <vincentkims49@gmail.com>
2023-08-09 15:53:48 +0300

    first commit

---
+++ Server/index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import imageGenRoutes from './routes/imageGenRoutes.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/imageGen', imageGenRoutes);
+
+app.get('/', async (req, res) => {
+  // res.status(200).json({
+  //   message: 'Hello from ImageGen!',
+  // });
+  res.send('Hello There');
+});
+
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => console.log('Server started on port http://localhost:8080'));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
